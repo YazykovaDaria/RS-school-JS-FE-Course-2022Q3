09844edc51a769d8bd82658f3861c2ce6e4b1335
YazykovaDaria <yazykovadasva@gmail.com>
2022-10-08 21:39:56 +0300

    refactor: add files

---
+++ online-zoo/src/js/data.js
@@ -0,0 +1,41 @@
+export const animal = [
+  {
+    id: 'an1', img: '../assets/img/penguin.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'Penguins', place: 'Native to Antarctica',
+  },
+  {
+    id: 'an2', img: '../assets/img/alligator.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'Alligators', place: 'Native to Southeastern U. S.',
+  },
+  {
+    id: 'an3', img: '../assets/img/cheetahs.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'cheetahs', place: 'Native to Africa',
+  },
+  {
+    id: 'an4', img: '../assets/img/sloth.jpg', icon: '../assets/icons/banana-bamboo_icon.svg', name: 'Two-toed Sloth', place: 'Mesoamerica, South America',
+  },
+  {
+    id: 'an5', img: '../assets/img/gorilla.jpg', icon: '../assets/icons/banana-bamboo_icon.svg', name: 'Gorillas', place: 'Native to Congo',
+  },
+  {
+    id: 'an6', img: '../assets/img/eagle.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'Eagles', place: 'Native to South America',
+  },
+  {
+    id: 'an7', img: '../assets/img/pandas.jpg', icon: '../assets/icons/banana-bamboo_icon.svg', name: 'giant Pandas', place: 'Native to Southwest China',
+  },
+  {
+    id: 'an8', img: '../assets/img/otter.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'otter', place: 'Native to South America',
+  },
+  {
+    id: 'an9', img: '../assets/img/wombat.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'wombat', place: 'Native to Australia',
+  },
+  {
+    id: 'an10', img: '../assets/img/red-panda.jpg', icon: '../assets/icons/banana-bamboo_icon.svg', name: 'red panda', place: 'Native to Southwest China',
+  },
+  {
+    id: 'an11', img: '../assets/img/manul.jpg', icon: '../assets/icons/meet-fish_icon.svg', name: 'manul', place: 'Native to Antarctica',
+  },
+  {
+    id: 'an12', img: '../assets/img/capibara.jpg', icon: '../assets/icons/banana-bamboo_icon.svg', name: 'capibara', place: 'Mesoamerica, South America',
+  },
+
+];
+
+export const testimonials = [];
