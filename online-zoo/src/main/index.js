45b5167e912512e7f65a2c35bfacdc01018c5e23
YazykovaDaria <yazykovadasva@gmail.com>
2022-10-10 00:11:55 +0300

    feat: add popup

--- online-zoo/src/main/index.js
+++ online-zoo/src/main/index.js
@@ -3,8 +3,6 @@ import 'normalize.css';
 import './style.scss';
 import burgerMenu from '../js/burger';
 import goCarousel from '../js/carousel';
-// import {animals} from '../js/data';
-// console.log(animals);
 
 burgerMenu();
 goCarousel();
