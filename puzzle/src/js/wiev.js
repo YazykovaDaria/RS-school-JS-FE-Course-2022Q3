94b6ffb69a8517527ca07dbbcabc157646d6bcda
YazykovaDaria <yazykovadasva@gmail.com>
2022-10-24 17:48:43 +0300

    refactor: add setSizeElement for change size

--- puzzle/src/js/wiev.js
+++ puzzle/src/js/wiev.js
@@ -17,30 +17,63 @@ const showMoves = (movesCount, movesEl) => {
   movesEl.innerText = movesCount;
 };
 
-//доделать стили + закрытие + проверить на нескольких выигрышах
-const showWonMessage = (isShow, {modal, movesCount, timesMinute, timesSecond} ) => {
-if (isShow) {
-const moves = movesCount.textContent;
-const minutes = timesMinute.textContent;
-const seconds = timesSecond.textContent;
-const mes = `Hooray! You solved the puzzle in ${minutes}:${seconds} and ${moves} moves!`;
-modal.innerText = mes;
-modal.classList.remove('hidden');
-} else {
-  modal.classList.add('hidden');
-}
-}
-
-const changeFrameSize = (size, {gamePlay}) => {
-  gamePlay.innerHTML = '';
-  const newItems = [];
-  for (let i = 0; i < size; i += 1) {
-    const item = `<button class="item" data-matrix-id="${i + 1}">${i + 1}</button>`;
-    newItems.push(item);
+// доделать стили + закрытие + проверить на нескольких выигрышах
+const showWonMessage = (isShow, {
+  modal, movesCount, timesMinute, timesSecond,
+}) => {
+  if (isShow) {
+    const moves = movesCount.textContent;
+    const minutes = timesMinute.textContent;
+    const seconds = timesSecond.textContent;
+    const mes = `Hooray! You solved the puzzle in ${minutes}:${seconds} and ${moves} moves!`;
+    modal.innerText = mes;
+    modal.classList.remove('hidden');
+  } else {
+    modal.classList.add('hidden');
   }
-  gamePlay.innerHTML = newItems.join('');
-  //console.log(gamePlay);
-}
+};
+
+const setSizeElement = (element, size) => {
+  let elementSize;
+
+  switch (size) {
+    case 9:
+      elementSize = '33.3%';
+      break;
+    case 16:
+      elementSize = '25%';
+      break;
+    case 25:
+      elementSize = '20%';
+      break;
+    case 36:
+      elementSize = '16.7%';
+      break;
+    case 49:
+      elementSize = '14.3%';
+      break;
+    case 64:
+      elementSize = '12.5%';
+      break;
+    default:
+      break;
+  }
+  element.style.width = elementSize;
+  element.style.height = elementSize;
+};
+
+const changeFrameSize = (size, { gamePlay }) => {
+  gamePlay.innerHTML = '';
+  gamePlay.classList.add('game-shuffle');
+  for (let i = 0; i < size; i += 1) {
+    const item = document.createElement('button');
+    item.classList.add('item');
+    item.setAttribute('data-matrix-id', `${i + 1}`);
+    item.textContent = `${i + 1}`;
+    setSizeElement(item, size);
+    gamePlay.append(item);
+  }
+};
 
 const appWiev = (state, elements) => onChange(state, (path, value) => {
   switch (path) {
@@ -48,12 +81,12 @@ const appWiev = (state, elements) => onChange(state, (path, value) => {
       stopWatch(value, elements.timesMinute, elements.timesSecond);
       break;
 
-      case 'isWin':
+    case 'isWin':
 
-  showWonMessage(value, elements);
+      showWonMessage(value, elements);
 
-        //console.log(value);
-        break;
+      // console.log(value);
+      break;
 
     case 'isShuffle':
       toggleShuffleClass(elements.gamePlay, value);
@@ -69,9 +102,9 @@ const appWiev = (state, elements) => onChange(state, (path, value) => {
       // console.log(value);
       break;
 
-      case 'gamePlay.countItems':
-changeFrameSize(value, elements);
-      //console.log(value);
+    case 'gamePlay.countItems':
+      changeFrameSize(value, elements);
+      // console.log(value);
       break;
 
     default:
