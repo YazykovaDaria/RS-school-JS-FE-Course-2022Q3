30e53b363c516fc8dc2eecf703ca70bb7f2e2b49
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-10 21:05:57 +0300

    feat: add metod + score

--- songbird/src/js/model/model.js
+++ songbird/src/js/model/model.js
@@ -5,15 +5,21 @@ const model = {
   data: birdsData,
   quiz: {
     level: 0,
+    score: 0,
     answers: [],
     rightAnswer: {},
-    isWin: false,
+    isWonLevel: false,
+    isWonGame: false,
   },
   startQuiz() {
     const roundAnswers = this.data.filter((item) => item.level === this.quiz.level);
     this.quiz.answers = roundAnswers;
     const randomIndex = getRandomInt(roundAnswers.length);
     this.quiz.rightAnswer = roundAnswers[randomIndex];
+  },
+  getAnswer(id) {
+    const answer = this.quiz.answers.filter((item) => item.id === id);
+    return answer[0];
   },
 };
 
