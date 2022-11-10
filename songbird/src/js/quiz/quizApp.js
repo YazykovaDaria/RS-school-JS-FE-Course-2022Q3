d34e20cb589cb8f6048f3c8867a36146498fbeea
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-10 21:07:24 +0300

    feat: add gameplay

--- songbird/src/js/quiz/quizApp.js
+++ songbird/src/js/quiz/quizApp.js
@@ -1,6 +1,11 @@
 import model from '../model/model';
 import viewQuiz from '../view/quizView';
 
+const answersContainer = document.getElementById('quiz-answers');
+const maxLevel = 5;
+const quizRules = 'Послушайте плеер\nВыберите птицу из списка';
+let scoreCount = 5;
+
 const buildQuizContent = () => {
   model.startQuiz();
   const { answers, rightAnswer } = model.quiz;
@@ -8,10 +13,45 @@ const buildQuizContent = () => {
   viewQuiz('answers', answers);
   // добавление мелодии в плеер
   viewQuiz('melody', rightAnswer);
+  // вставка в блок выбранных птиц
+  viewQuiz('choised', quizRules);
+};
+
+const checkWonGame = (answer) => {
+  const { level } = model.quiz;
+  model.quiz.level += 1;
+  model.quiz.score += scoreCount;
+  scoreCount = 5;
+  viewQuiz('score', model.quiz.score);
+
+  if (level < maxLevel) {
+    viewQuiz('right', answer);
+  } else {
+    // здесь будет загрузка страницы с выигрышем
+    console.log('win');
+  }
 };
 
 const quizApp = () => {
   document.addEventListener('DOMContentLoaded', buildQuizContent);
+
+  answersContainer.addEventListener('click', (e) => {
+    const elId = Number(e.target.id);
+    const { id } = model.quiz.rightAnswer;
+
+    if (elId === id && !model.quiz.isWonLevel) {
+      model.quiz.isWonLevel = true;
+      checkWonGame(model.quiz.rightAnswer);
+    } else {
+      const answer = model.getAnswer(elId);
+      if (model.quiz.isWonLevel) {
+        viewQuiz('choised', answer);
+      } else {
+        scoreCount -= 1;
+        viewQuiz('wrong', answer);
+      }
+    }
+  });
 };
 
 export default quizApp;
--- songbird/src/js/view/quizView.js
+++ songbird/src/js/view/quizView.js
@@ -1,3 +1,7 @@
+import { playAudio } from '../helpers/helpers';
+import audioFail from '../../assets/audio/sucsses.mp3';
+import audioSucsses from '../../assets/audio/wrong.mp3';
+
 const questContainer = document.getElementById('question');
 const answersContainer = document.getElementById('quiz-answers');
 const choisedContainer = document.getElementById('choisedAnswer');
@@ -12,24 +16,67 @@ const buildAnswers = (answers) => {
     const li = document.createElement('li');
     li.classList.add('answer__list');
     li.setAttribute('id', `${item.id}`);
-    li.textContent = item.name;
+    const span = document.createElement('span');
+    span.textContent = item.name;
+    li.append(span);
     answersContainer.prepend(li);
   });
 };
 
 const changeAudio = ({ audio }) => audioSrc.src = audio;
 
+const showChoizedContent = (content) => {
+  choisedContainer.innerHTML = '';
+  if (typeof content === 'string') {
+    choisedContainer.textContent = content;
+  } else {
+    choisedContainer.textContent = content.name;
+    // тут будет добавление карточки птицы
+    // console.log(content);
+  }
+};
 
-const viewQuiz = (indicator, data) => {
+const addClassForAnswer = ({ id }, className) => {
+  const el = document.getElementById(String(id));
+  console.log(el);
+  el.classList.add(className);
+};
+
+const viewQuiz = (indicator, data = '') => {
   switch (indicator) {
     case 'answers':
       buildAnswers(data);
+      btn.disabled = true;
       break;
 
     case 'melody':
-      //поменять логику после переделки плеера
+      // поменять логику после переделки плеера
       changeAudio(data);
+      console.log(data);
+      break;
 
+    case 'wrong':
+      addClassForAnswer(data, indicator);
+      playAudio(audioSucsses);
+      showChoizedContent(data);
+      break;
+
+    case 'right':
+      addClassForAnswer(data, indicator);
+      playAudio(audioFail);
+      showChoizedContent(data);
+      btn.disabled = false;
+      // функцию для показа птицы в игровой карточке
+
+      // console.log(data);
+      break;
+
+    case 'choised':
+      showChoizedContent(data);
+      break;
+
+    case 'score':
+      score.textContent = data;
       break;
 
     default:
