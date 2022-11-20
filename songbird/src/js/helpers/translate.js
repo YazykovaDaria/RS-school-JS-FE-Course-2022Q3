9ae25e1b7f1f34b19829a6adde9fb04b90170e48
YazykovaDaria <yazykovadasva@gmail.com>
2022-11-21 00:40:03 +0300

    feat: add translate

--- songbird/src/js/helpers/translate.js
+++ songbird/src/js/helpers/translate.js
@@ -1,6 +1,13 @@
+import dictionary from '../model/dictionary';
 
 const translate = (lang) => {
-  console.log('translate ' + lang);
-}
+  const els = document.querySelectorAll('.js-lang');
+  els.forEach((el) => {
+    const key = el.dataset.lang;
+    // eslint-disable-next-line no-param-reassign
+    el.textContent = dictionary[key][lang];
+    //console.log(dictionary[lang], lang);
+  });
+};
 
 export default translate;
