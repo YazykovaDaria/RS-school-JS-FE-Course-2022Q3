fd909b8d8a13cddc1d4fb95d29663d8d9a5e4434
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-10 15:58:34 +0300

    fix: change configs

--- newsJs/webpack.config.js
+++ newsJs/webpack.config.js
@@ -1,29 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const baseConfig = {
-  entry: path.resolve(__dirname, "./src/index.ts"),
+  entry: "./src/index.ts",
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
   output: {
-    filename: "index.js",
-    path: path.resolve(__dirname, "../dist"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -32,6 +21,28 @@ const baseConfig = {
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+      { test: /\.js$/, loader: "source-map-loader" },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  },
 };
 
 module.exports = ({ mode }) => {
