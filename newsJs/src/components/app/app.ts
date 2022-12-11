d059148a1eeeed85ab3485223309eee47d771c82
YazykovaDaria <yazykovadasva@gmail.com>
2022-12-11 18:46:32 +0300

    refactor: add types

--- newsJs/src/components/app/app.ts
+++ newsJs/src/components/app/app.ts
@@ -3,15 +3,17 @@ import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
 
 class App {
+    private controller: AppController;
+    private view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start(): void {
-        document.querySelector('.sources').addEventListener('click', (e): void =>
+        document.querySelector('.sources')?.addEventListener('click', (e): void =>
             this.controller.getNews(e, (data: NewsResponse) => {
-                console.log(data);
                 this.view.drawNews(data);
             })
         );
