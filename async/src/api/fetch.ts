508d2db9d96826ee13b6df47a4d70109ddf36d55
YazykovaDaria <yazykovadasva@gmail.com>
2023-01-19 13:02:26 +0300

    feat: winners api

--- async/src/api/fetch.ts
+++ async/src/api/fetch.ts
@@ -1,23 +1,28 @@
-import { Car, Cars, CreateCar } from './../types/types';
+import {
+  Car, Cars, CreateCar, Winners, Winner, CreateWinner
+} from '../types/types';
 
 const baseUrl = 'http://127.0.0.1:3000';
 
-export const getCars = async(): Promise<Cars | null> => {
+export const getCars = async (
+  page = 1,
+  limit = 7,
+): Promise<{ cars: Cars; count: string } | null> => {
   try {
-    const res = await fetch(`${baseUrl}/garage`);
+    const res = await fetch(`${baseUrl}/garage?_limit=${limit}&_page=${page}`);
     if (res.status === 200) {
-      const data: Cars = await res.json();
-      return data;
+      const cars: Cars = await res.json();
+      const count = res.headers.get('X-Total-Count') || '0';
+      return { cars, count };
     }
 
     return null;
   } catch (err) {
     throw new Error(err);
   }
-}
+};
 
-
-export const getCar = async(carId: number): Promise<Car | null> => {
+export const getCar = async (carId: number): Promise<Car | null> => {
   try {
     const res = await fetch(`${baseUrl}/garage/${carId}`);
     if (res.status === 200) {
@@ -29,11 +34,11 @@ export const getCar = async(carId: number): Promise<Car | null> => {
   } catch (err) {
     throw new Error(err);
   }
-}
+};
 
-export const createCar = async(car: CreateCar): Promise<Car | null> => {
+export const createCar = async (car: CreateCar): Promise<Car | null> => {
   try {
-  const res =  await fetch(`${baseUrl}/garage`, {
+    const res = await fetch(`${baseUrl}/garage`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -45,13 +50,12 @@ export const createCar = async(car: CreateCar): Promise<Car | null> => {
       return data;
     }
     return null;
-
   } catch (err) {
     throw new Error(err);
   }
-}
+};
 
-export const deleteCar = async(carId: number): Promise<void> => {
+export const deleteCar = async (carId: number): Promise<void> => {
   try {
     await fetch(`${baseUrl}/garage/${carId}`, {
       method: 'DELETE',
@@ -59,12 +63,11 @@ export const deleteCar = async(carId: number): Promise<void> => {
   } catch (err) {
     throw new Error(err);
   }
-}
+};
 
-
-export const updateCar = async(carId: number, car: CreateCar):Promise<void> => {
+export const updateCar = async (carId: number, car: CreateCar): Promise<void> => {
   try {
-  await fetch(`${baseUrl}/garage/:${carId}`, {
+    await fetch(`${baseUrl}/garage/${carId}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -74,4 +77,84 @@ export const updateCar = async(carId: number, car: CreateCar):Promise<void> => {
   } catch (err) {
     throw new Error(err);
   }
-}
+};
+
+export const getWinners = async (
+  page = 1,
+  sort = 'time',
+  order = 'ASC',
+  limit = 10,
+): Promise<{ winners: Winners; count: string } | null> => {
+  try {
+    const res = await fetch(
+      `${baseUrl}/winners?_page=${page}&_limit=${limit}&_sort=${sort}&_order=${order}`,
+    );
+
+    if (res.status === 200) {
+      const winners = await res.json();
+      const count = res.headers.get('X-Total-Count') || '0';
+      return { winners, count };
+    }
+
+    return null;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const getWinner = async (carId: number): Promise<Winner | null> => {
+  try {
+    const res = await fetch(`${baseUrl}/winners/${carId}`);
+    if (res.status === 200) {
+      const data = await res.json();
+      return data;
+    }
+
+    return null;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const deleteWinner = async (carId: number):Promise<void> => {
+  try {
+    await fetch(`${baseUrl}/winners/${carId}`, {
+      method: 'DELETE',
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const createWinner = async (winner: Winner): Promise<Winner | null> => {
+  try {
+    const res = await fetch(`${baseUrl}/winners`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(winner),
+    });
+    if (res.status === 201) {
+      const data = await res.json();
+      return data;
+    }
+    return null;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const updateWinner = async (carId: number, winner: CreateWinner): Promise<void> => {
+  try {
+    await fetch(`${baseUrl}/winners/${carId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(winner),
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
