bd3d396ec69cbd6857a6368866f692d2effee4fd
YazykovaDaria <93446641+YazykovaDaria@users.noreply.github.com>
2023-03-13 10:48:48 +0300

    refactor: change deploy adress

--- newsJs/src/components/controller/appLoader.ts
+++ newsJs/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', {
+        super('https://YazykovaDaria.github.io/RS-school-JS-FE-Course-2022Q3/newsJs/', {
             apiKey: 'b9dd86b9fad441bebccfc4b505a7c1bd',
         });
     }
